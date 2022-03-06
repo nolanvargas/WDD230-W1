1c507d7ddec671842997a37b3da2e93643d000a2
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-03-05 18:14:34 -0700

    minor updates

--- lesson2/index.js
+++ lesson2/index.js
@@ -2,4 +2,4 @@ var string = document.lastModified;
 var year = new Date().getFullYear()
 document.querySelector("#footerB").innerHTML = `Last Updated: ${string}`;
 document.querySelector("#footerA").innerHTML = 
-    `&copy; ${year} | Nolan Thomas Vargas | Idaho | <a href="https://www.byui.edu/online" style="color: #B1E4FB">BYUI Online Learning</a>`
+    `&copy; ${year} | RN Weather | Nolan Thomas Vargas`
