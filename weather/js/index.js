1531ed4d867a62a03c14a85ca5743868f9edc51c
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-03-05 18:13:41 -0700

    minor updates

--- weather/js/index.js
+++ weather/js/index.js
@@ -1,7 +1,10 @@
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
+const string = document.lastModified;
+const year = new Date().getFullYear()
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-// To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+document.querySelector("#footerB").innerHTML = `Last Updated: ${string}`;
+document.querySelector("#footerA").innerHTML = 
+    `&copy; ${year} | RN Weather | Nolan Thomas Vargas`
