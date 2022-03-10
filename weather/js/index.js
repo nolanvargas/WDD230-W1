299e2562edc72d2199307e2f7173522e7b1cbae7
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-03-10 09:38:41 -0700

    .

--- weather/js/index.js
+++ weather/js/index.js
@@ -1,10 +1,16 @@
 const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
+const mainnav = document.querySelector('.navigation');
 const string = document.lastModified;
-const year = new Date().getFullYear()
+const year = new Date().getFullYear();
+const day = new Date().getDay();
+const prestonPancakes = document.querySelector('#prestonPancakes');
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 document.querySelector("#footerB").innerHTML = `Last Updated: ${string}`;
-document.querySelector("#footerA").innerHTML = 
-    `&copy; ${year} | RN Weather | Nolan Thomas Vargas`
+document.querySelector("#footerA").innerHTML = `&copy; ${year} | RN Weather | Nolan Thomas Vargas`;
+
+console.log(prestonPancakes.style.display)
+
+if (day != 5) { prestonPancakes.style.display = "none" }
+else { prestonPancakes.style.display = "flex" }
