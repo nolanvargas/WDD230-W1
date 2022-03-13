61872fa8e0de07c671e80e45ce489a6f2540fded
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-03-12 20:29:13 -0700

    .

---
+++ weather/js/windchill.js
@@ -0,0 +1,11 @@
+currentTemp = parseInt(document.querySelector('#currentTemp').innerHTML);
+windSpeed = parseInt(document.querySelector('#windSpeed').innerHTML);
+
+if (windSpeed > 4.8 && currentTemp < 50) {
+    var windChill = (35.74 + (0.6215 * currentTemp))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275*currentTemp*Math.pow(windSpeed,0.16));
+    var windChill= Math.round(windChill);
+    document.querySelector('#windChill').innerHTML = parseInt(windChill) + '°';
+} else {
+    document.querySelector('#windChill').innerHTML = 'N/A';
+}
+
